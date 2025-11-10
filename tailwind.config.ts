74e1669d37d414f8dc152da45f11e101a9c0a8c2
LukaM25 <luka.matanic@icloud.com>
2025-11-10 18:56:05 +0100

    neue landingpage

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,20 @@
 import type { Config } from 'tailwindcss';
 
-export default {
+const config: Config = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}', // if you still have pages/
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}', // only if you still have /pages
+    './stories/**/*.{js,ts,jsx,tsx,mdx}', // optional
   ],
   theme: {
     extend: {
       fontFamily: { sans: ['var(--font-sans)'] },
     },
   },
+  safelist: [{ pattern: /^(bg|text|border)-(gray|sky|blue|black)-(50|100|200|300|400|500|600|700|800|900)$/ }],
+
   plugins: [],
-} satisfies Config;
+};
+
+export default config;
